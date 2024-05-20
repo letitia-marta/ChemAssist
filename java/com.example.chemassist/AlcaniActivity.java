1b14b72facd01f458896042bb5775ada819f58d1
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2024-05-20 20:53:40 +0300

    Create AlcaniActivity.java

---
+++ java/com.example.chemassist/AlcaniActivity.java
@@ -0,0 +1,18 @@
+package com.example.chemassist;
+
+import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.widget.ImageView;
+        import android.widget.LinearLayout;
+        import android.widget.TextView;
+        import androidx.core.content.ContextCompat;
+
+public class AlcaniActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alcani);
+    }
+}
