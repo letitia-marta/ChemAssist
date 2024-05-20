d88c614763ba8d3c2af6eab580ec8a5969f086a1
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2024-05-20 20:55:44 +0300

    Create AreneActivity.java

---
+++ java/com.example.chemassist/AreneActivity.java
@@ -0,0 +1,14 @@
+package com.example.chemassist;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AreneActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arene);
+    }
+}
